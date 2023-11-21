af79f8798477439d81ad1c9c9f47e813fd37710f
lucasBmm <lucassbomfimm@gmail.com>
2023-11-21 11:45:13 -0300

    adicionado codigo php

--- frontend/index.php
+++ frontend/index.php
@@ -3,7 +3,7 @@
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -51,6 +51,26 @@
                 <input type="number" name="anos-luz" id="anos-luz" required>
                 <input type="submit" name="converter-anos-luz" value="Converter">
             </form>
+            <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["converter-anos-luz"])) {
+                    $anosLuzValue = $_POST["anos-luz"];
+                    
+                    if ($anosLuzValue >= 1) {
+                        // Faz a requisição para localhost com path /netcon/api/v1/converter?anos-luz=1
+                        $url = "$base_url?anos-luz=" . urlencode($anosLuzValue);
+                        $response = file_get_contents($url);
+                        $data = json_decode($response, true);
+        
+                        if ($data) {
+                            echo "<p>$anosLuzValue anos-luz é aproximadamente {$data['value']}.</p>";
+                        } else {
+                            echo "<p style='color: red;'>Erro na requisição.</p>";
+                        }
+                    } else {
+                        echo "<p style='color: red;'>Erro: Insira um valor maior ou igual a 1.</p>";
+                    }
+                }
+            ?>
         </section>
     
         <section style="right: 5%;">
@@ -60,6 +80,28 @@
                 <input type="number" name="km" id="km" required>
                 <input type="submit" name="converter-km" value="Converter">
             </form>
+            <?php
+                if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["converter-km"])) {
+                    $kmValue = $_POST["km"];
+
+                    if ($kmValue >= 1) {
+                        // Faz a requisição para localhost com path /netcon/api/v1/converter?km=1
+                        $url = "$base_url?km=" . urlencode($kmValue);
+                        $response = file_get_contents($url);
+                        $data = json_decode($response, true);
+
+                        echo '<p style="height: 20px;">';
+                        if ($data) {
+                            echo "Resultado: <span id='result'>{$data['value']}</span>";
+                        } else {
+                            echo "<span style='color: red;'>Erro na requisição.</span>";
+                        }
+                        echo '</p>';
+                    } else {
+                        echo "<p style='color: red;'>Erro: Insira um valor maior ou igual a 1.</p>";
+                    }
+                }
+            ?>
         </section>
     </div>
 </body>
